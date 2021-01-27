f22ee4c9cc28a9a6df0458cbc3465961a8c439d9
busrabkn <74192539+busrabkn@users.noreply.github.com>
2021-01-27 08:05:59 -0500

    movie2 branch work a lot

--- src/main/java/HelloClass.java
+++ src/main/java/HelloClass.java
@@ -2,5 +2,8 @@ public class HelloClass {
     public static void main(String[] args) {
         System.out.println("hello world b22");
         System.out.println("try1");
+        //1
+        //2
+        //3
     }
 }
